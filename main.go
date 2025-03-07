e5e29878d1814162269e4cc0e2741aec1a0e54f0
Julien Doutre <jul.doutre@gmail.com>
2025-03-07 20:01:03 +0100

    init

---
+++ main.go
@@ -0,0 +1,59 @@
+package cryptopals
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"math"
+	"unicode"
+)
+
+func HexToBase64(input string) (string, error) {
+	raw, err := hex.DecodeString(input)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.StdEncoding.EncodeToString(raw), nil
+}
+
+func Xor(a, b []byte) []byte {
+	out := make([]byte, len(a))
+
+	for i := range a {
+		out[i] = a[i] ^ b[i]
+	}
+
+	return out
+}
+
+func Block(a byte, length int) []byte {
+	out := make([]byte, length)
+
+	for i := range out {
+		out[i] = a
+	}
+
+	return out
+}
+
+func IsEnglish(input string) bool {
+	for _, c := range input {
+		if !(unicode.IsLetter(c) || unicode.IsSpace(c) || unicode.IsDigit(c) || c == '\'') {
+			return false
+		}
+	}
+
+	return true
+}
+
+func CrackSingleCharXor(ciphertext []byte) (byte, string, bool) {
+	for candidate := range math.MaxUint8 {
+		plaintext := string(Xor(ciphertext, Block(byte(candidate), len(ciphertext))))
+
+		if IsEnglish(plaintext) {
+			return byte(candidate), plaintext, true
+		}
+	}
+
+	return 0, "", false
+}
---
+++ main_test.go
@@ -0,0 +1,75 @@
+package cryptopals_test
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/hex"
+	"testing"
+
+	_ "embed"
+
+	"github.com/juliendoutre/cryptopals"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+//go:embed data/4.txt
+var data string
+
+func TestSet1(t *testing.T) {
+	t.Parallel()
+
+	// https://cryptopals.com/sets/1/challenges/1
+	t.Run("challenge 1", func(t *testing.T) {
+		t.Parallel()
+
+		actual, err := cryptopals.HexToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+		require.NoError(t, err)
+
+		assert.Equal(t, "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t", actual)
+	})
+
+	// https://cryptopals.com/sets/1/challenges/2
+	t.Run("challenge 2", func(t *testing.T) {
+		t.Parallel()
+
+		a, err := hex.DecodeString("1c0111001f010100061a024b53535009181c")
+		require.NoError(t, err)
+
+		b, err := hex.DecodeString("686974207468652062756c6c277320657965")
+		require.NoError(t, err)
+
+		assert.Equal(t, "746865206b696420646f6e277420706c6179", hex.EncodeToString(cryptopals.Xor(a, b)))
+	})
+
+	// https://cryptopals.com/sets/1/challenges/3
+	t.Run("challenge 3", func(t *testing.T) {
+		t.Parallel()
+
+		ciphertext, err := hex.DecodeString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+		require.NoError(t, err)
+
+		_, plaintext, isCracked := cryptopals.CrackSingleCharXor(ciphertext)
+		assert.True(t, isCracked)
+		assert.Equal(t, "Cooking MC's like a pound of bacon", plaintext)
+	})
+
+	// https://cryptopals.com/sets/1/challenges/4
+	t.Run("challenge 4", func(t *testing.T) {
+		t.Parallel()
+
+		scanner := bufio.NewScanner(bytes.NewBufferString(data))
+
+		for scanner.Scan() {
+			ciphertext, err := hex.DecodeString(scanner.Text())
+			require.NoError(t, err)
+
+			_, plaintext, isCracked := cryptopals.CrackSingleCharXor(ciphertext)
+			if isCracked {
+				assert.Equal(t, "Now that the party is jumping\n", plaintext)
+			}
+		}
+
+		assert.NoError(t, scanner.Err())
+	})
+}
